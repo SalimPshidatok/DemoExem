377095658130206a5c23af7d8a3db5015e64e343
TimkaMaksPro <timka77745tim@gmail.com>
2023-03-31 16:22:35 +0300

    main

---
+++ ViewModel/AuthWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craft.ViewModel
+{
+    public class AuthWindowViewModel:BaseViewModel
+    {
+        private string _login;
+        private string _password;
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(Login))
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                Password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+    }
+}
